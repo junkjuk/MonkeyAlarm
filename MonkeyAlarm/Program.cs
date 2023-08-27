22ca1251fa8b0a0f323ee52ee5af7f4c3fb72db6
KosieVich <junkjuk5645@gmail.com>
2023-08-27 20:41:53 +0300

    init commit

---
+++ MonkeyAlarm/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LibGit2Sharp;
+
+Console.WriteLine("Hello, World!");
+Repository.Clone("https://github.com/EdiWang/EnvSetup.git", "./");
+
